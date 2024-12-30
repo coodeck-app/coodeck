1df5d3e002bc966843a13d7a682a017549301761
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-12-30 17:01:21 +0100

    🧹 clean up layout component by removing unnecessary line breaks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
+'use client';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
-
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -54,9 +53,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider theme={theme}>
           <Header />
           <div className="animated-gradient">
